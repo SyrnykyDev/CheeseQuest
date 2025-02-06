5ac6e53cd9d195412e9defda865dc79b8aea7488
mykyda <nikita.danech@gmail.com>
2025-02-06 10:12:12 +0200

    init

---
+++ src/main/java/com/mykyda/CheeseQuestApplication.java
@@ -0,0 +1,13 @@
+package com.mykyda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheeseQuestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mykyda/CheeseQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykyda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseQuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
