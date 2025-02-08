5845a40cd5547d5a0cf3454d188c0d5039f8e12c
Dmytro <20IvanovDmitriy03@gmail.com>
2025-02-08 14:41:15 +0200

    modified: input

--- web/src/components/Input/Input.tsx
+++ web/src/components/Input/Input.tsx
@@ -1,35 +1,61 @@
-"use client"
-
+"use client";
 
 // *** COMPONENTS ***
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from "react";
 
 // *** STYLES ***
-import styles from './Input.module.scss'
+import styles from "./Input.module.scss";
 
-
-interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
-    title?: string;
+interface InputProps extends React.InputHTMLAttributes<any> {
+  title?: string;
+  inputType?: "sans";
+  value?: string | number;
+  required?: boolean;
 }
-
 
 /**
  * Extended default input
  **/
-const Input = (props:InputProps) => {
+const Input = (props: InputProps) => {
+  const { onChange, required } = props;
 
-    const {onChange} = props;
+  const [change, setChange] = useState(true);
+  const [state, setState] = useState("");
 
-    const onMiddleChange = (event:React.FormEvent<HTMLInputElement>):void=>{
-        if(onChange) onChange(event);
+  const onMiddleChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    console.log(event?.target?.value);
+    if (onChange) {
+      onChange(event);
+    } else {
+      setState(event?.target?.value);
     }
-    return (
-        <div className={styles.input}>
-            {props?.title &&<span>{props.title}</span>}
+  };
 
-            <input className={styles.input_input} {...props} onChange={onMiddleChange}/>
-        </div>
-    );
+  useEffect(() => {
+    const changeStage = () => {
+      setChange(true);
+      setTimeout(() => {
+        setChange(false);
+      }, 150); // Adjust the timeout duration equal to your animation time, in your case is 1100
+    };
+    changeStage();
+  }, [state]);
+
+  return (
+    <div className={`${styles.input} ${styles.input_sans}`}>
+      {props?.title && <span>{props.title}</span>}
+
+      <input
+        className={`${styles.input_input} ${change ? styles.input_anim : ""}`}
+        {...props}
+        placeholder={
+          props?.placeholder && `${props?.placeholder} ${required && "*"}`
+        }
+        value={state}
+        onChange={onMiddleChange}
+      />
+    </div>
+  );
 };
 
 export default Input;
