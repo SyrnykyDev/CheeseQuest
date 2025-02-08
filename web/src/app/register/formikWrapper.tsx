c0cec2821ce5c448a2e277929dcc062369f75304
Dmytro <20IvanovDmitriy03@gmail.com>
2025-02-08 14:42:35 +0200

    some stuff...

---
+++ web/src/app/register/formikWrapper.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { ReactNode } from "react";
+import { Formik } from "formik";
+
+const FormikWrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => <form onSubmit={handleSubmit}>{children}</form>}
+    </Formik>
+  );
+};
+
+export default FormikWrapper;
