f2c3ba55119547667df62b14e1228aba473ebe82
Dmytro <20IvanovDmitriy03@gmail.com>
2025-02-08 14:36:17 +0200

    modified: header - added logo and buttons

--- web/src/components/header/header.tsx
+++ web/src/components/header/header.tsx
@@ -7,17 +7,28 @@ import styles from "./header.module.scss";
 
 // *** ASSETS ***
 import userIMG from "../../../public/header/user.svg";
+import logoSVG from "../../../public/header/logo.svg";
 import Link from "next/link";
+import Button from "@/components/Button/Button";
 
 const Header = () => {
   return (
     <>
       <header className={styles.header}>
-        <Link href="/">LOGO</Link>
-
+        <Link href="/">
+          <Image src={logoSVG} alt={"Logo"} />
+        </Link>
+        <div className={styles.header_rightPart}>
+          <Button>Sign In</Button>
+          <Link href={"/login"}>
+            <Button buttonColor={"blue"}>Log In</Button>
+          </Link>
+          <Button buttonColor={"orange"}>Log In</Button>
+          {/*<Button>Sign In</Button>*/}
+        </div>
         <div>
           <Link href="/profile">
-            <Image src={userIMG} alt={"User Img"} width={30} height={30} />
+            {/*<Image src={userIMG} alt={"User Img"} width={30} height={30} />*/}
           </Link>
         </div>
       </header>
