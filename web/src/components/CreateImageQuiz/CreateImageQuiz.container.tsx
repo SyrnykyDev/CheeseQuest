1a75f9e92876d655d9f99e3c16b6a3375c1f8063
Dmytro <20IvanovDmitriy03@gmail.com>
2025-02-11 11:24:01 +0200

    Modified: CreateImageQuiz

--- web/src/components/CreateImageQuiz/CreateImageQuiz.container.tsx
+++ web/src/components/CreateImageQuiz/CreateImageQuiz.container.tsx
@@ -1,16 +1,25 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CreateImageQuizContainer = () => {
-  const [position, setPosition] = useState({ x: 100, y: 0 });
-  const [position2, setPosition2] = useState({ x: 250, y: 150 });
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [elemParams, setElemParams] = useState({ width: 250, height: 150 });
+  const [position2, setPosition2] = useState({
+    x: elemParams.width,
+    y: elemParams.height,
+  });
 
+  const [disabled, setDisabled] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    setElemParams({
+      width: position2.x - position.x,
+      height: position2.y - position.y,
+    });
+  }, [position2, position]);
   const onDrag = (props: any) => {
-    console.log(props);
-    let img = new Image();
-    img.src =
-      "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
-    props.dataTransfer.setDragImage(img, 0, 0);
+    if (props.clientX > 0 && props.clientY > 0) console.log(props);
 
     if (props.clientX > 0 && props.clientY > 0)
       setPosition({
@@ -21,12 +30,8 @@ const CreateImageQuizContainer = () => {
   };
 
   const onDrag2 = (props: any) => {
-    console.log(props);
+    console.log(props.clientX, props.clientY);
 
-    let img = new Image();
-    img.src =
-      "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
-    props.dataTransfer.setDragImage(img, 0, 0);
     if (props.clientX > 0 && props.clientY > 0)
       setPosition2({
         x: props.clientX,
@@ -34,9 +39,43 @@ const CreateImageQuizContainer = () => {
       });
     // setPosition();
   };
+
+  const onDragSizeStart = () => {
+    setDisabled(true);
+  };
+
+  const onDragSizeEnd = () => {
+    setDisabled(false);
+  };
+
+  const onElemDrag = (props: any) => {
+    // if (props.clientX > 0 && props.clientY > 0)
+    if (disabled) return;
+    setMousePosition({
+      x: props.screenX - props.clientX,
+      y: props.screenY - props.clientY,
+    });
+    if (props.clientX > 0 && props.clientY > 0)
+      setPosition({
+        x: props.clientX - elemParams.width / 2,
+        y: props.clientY - 70 - elemParams.height / 2,
+      });
+    if (props.clientX > 0 && props.clientY > 0)
+      setPosition2({
+        x: props.clientX + elemParams.width - elemParams.width / 2,
+        y: props.clientY - 70 + elemParams.height - elemParams.height / 2,
+      });
+  };
   return {
-    functions: { setPosition, onDrag, onDrag2 },
-    states: { position, position2 },
+    functions: {
+      setPosition,
+      onDrag,
+      onDrag2,
+      onElemDrag,
+      onDragSizeStart,
+      onDragSizeEnd,
+    },
+    states: { position, position2, elemParams },
   };
 };
 
--- web/src/components/CreateImageQuiz/CreateImageQuiz.tsx
+++ web/src/components/CreateImageQuiz/CreateImageQuiz.tsx
@@ -2,72 +2,88 @@
 import React from "react";
 import createImageQuizContainer from "@/components/CreateImageQuiz/CreateImageQuiz.container";
 
+import styles from "./CreateImageQuiz.module.scss";
+
 const CreateImageQuiz = () => {
   const {
-    states: { position, position2 },
-    functions: { setPosition, onDrag, onDrag2 },
+    states: { position, position2, elemParams },
+    functions: {
+      setPosition,
+      onDrag,
+      onDrag2,
+      onElemDrag,
+      onDragSizeStart,
+      onDragSizeEnd,
+    },
   } = createImageQuizContainer();
   return (
     <div
       style={{
         position: "relative",
-        // width: "500px",
-        // height: "500px",
+        // width: "50px",
+        // height: "100%",
         // margin: "auto",
+        // display: "flex",
+        // backgroundColor: "red",
       }}
     >
       <div
         style={{
-          width: position2.x - position.x,
-          height: position2.y - position.y,
+          width: elemParams.width,
+          height: elemParams.height,
 
           position: "absolute",
           left: position.x,
-          bottom: -position2.y,
-          backgroundColor: "yellow",
+          top: position.y,
           cursor: "pointer",
+        }}
+        className={styles.createImageQuiz_highlightBox}
+        draggable
+        onDrag={onElemDrag}
+        onDragStart={(event) => {
+          let img = new Image();
+          img.src =
+            "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
+          event.dataTransfer.setDragImage(img, 0, 0);
         }}
       >
         <div
           draggable
           onDrag={onDrag}
           onDragStart={(event) => {
+            onDragSizeStart();
+
             let img = new Image();
             img.src =
               "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
             event.dataTransfer.setDragImage(img, 0, 0);
           }}
+          onDragEnd={onDragSizeEnd}
           style={{
             position: "absolute",
-            left: -30,
-            top: -30,
-            // left: position.x,
-            // top: position.y,
-            width: "40px",
-            height: "40px",
-            backgroundColor: "green",
-            cursor: "pointer",
+            left: -15,
+            top: -15,
           }}
+          className={styles.createImageQuiz_leftSizeOpt}
         />
 
         <div
           draggable
           onDrag={onDrag2}
+          onDragEnd={onDragSizeEnd}
           onDragStart={(event) => {
+            onDragSizeStart();
+
             let img = new Image();
             img.src =
               "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
             event.dataTransfer.setDragImage(img, 0, 0);
           }}
           style={{
-            position: "absolute",
-            right: -30,
-            bottom: -30,
-            width: "40px",
-            height: "40px",
-            backgroundColor: "red",
-            cursor: "pointer",
+            right: -15,
+            bottom: -15,
           }}
+          className={styles.createImageQuiz_rightSizeOpt}
         />
       </div>
     </div>
